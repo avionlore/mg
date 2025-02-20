67e35aefb705f930b8cc1247860588ac5a2be009
avionlore <coolblue.cst@gmx.de>
2025-02-20 21:57:07 +0100

    Added mission names

--- src/components/MissionSingle.vue
+++ src/components/MissionSingle.vue
@@ -1,12 +1,20 @@
 <template>
 <div>
     <p>Mission # {{ missionNumber }}</p>
+    <p>{{ missionName }}</p>
 </div>
 </template>
 <script>
+import missions from '../logics/Missions.js'
 
 export default {
     props: ['missionNumber'],
+    data() {
+        return {
+            missionName: missions.mission[Math.floor(Math.random() * missions.mission.length)].missionName
+        }
+        
+    }
 }
 </script>
 <style></style>
