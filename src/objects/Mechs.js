4f815bb848af8af5a5af84760d91ce9591d75876
avionlore <coolblue.cst@gmx.de>
2025-03-23 22:12:53 +0100

    Added some light and medium mechs

--- src/objects/Mechs.js
+++ src/objects/Mechs.js
@@ -10,15 +10,75 @@ export default {
     fullName: "Jenner",
     shortName: "Jenner",
     mechClass: MechClasses[1],
+  },  
+  Panther: {
+    fullName: "Panther",
+    shortName: "Panther",
+    mechClass: MechClasses[1],
+  },  
+  Mercury: {
+    fullName: "Mercury",
+    shortName: "Mercury",
+    mechClass: MechClasses[1],
+  },  
+  Urban: {
+    fullName: "Urban Mech",
+    shortName: "Urban",
+    mechClass: MechClasses[1],
+  },  
+  Raven: {
+    fullName: "Raven",
+    shortName: "Raven",
+    mechClass: MechClasses[1],
+  },
+  Sentinel: {
+    fullName: "Sentinel",
+    shortName: "Sentinel",
+    mechClass: MechClasses[1],
   },
   Wolverine: {
     fullName: "Wolverine",
     shortName: "Wolverine",
     mechClass: MechClasses[2],
   },
+  Hatchetman: {
+    fullName: "Hatchetman",
+    shortName: "Hatchetman",
+    mechClass: MechClasses[2],
+  },
   Hunchback: {
     fullName: "Hunchback",
     shortName: "Hunchback",
+    mechClass: MechClasses[2],
+  },
+  Enforcer: {
+    fullName: "Enforcer",
+    shortName: "Enforcer",
+    mechClass: MechClasses[2],
+  },
+  Centurion: {
+    fullName: "Centurion",
+    shortName: "Centurion",
+    mechClass: MechClasses[2],
+  },
+  ShadowHawk: {
+    fullName: "ShadowHawk",
+    shortName: "ShadowHawk",
+    mechClass: MechClasses[2],
+  },
+  Blackjack: {
+    fullName: "Blackjack",
+    shortName: "Blackjack",
+    mechClass: MechClasses[2],
+  },
+  PhoenixHawk: {
+    fullName: "PhoenixHawk",
+    shortName: "PhoenixHawk",
+    mechClass: MechClasses[2],
+  },
+  Griffin: {
+    fullName: "Griffin",
+    shortName: "Griffin",
     mechClass: MechClasses[2],
   },
   Orion: {
