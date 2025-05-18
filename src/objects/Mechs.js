bedac11b66d01e1d3b0fce91111fa9ff28f9c383
avionlore <avionlore@gmail.com>
2025-05-18 10:05:50 +0200

    Added missing mechs

--- src/objects/Mechs.js
+++ src/objects/Mechs.js
@@ -10,22 +10,22 @@ export default {
     fullName: "Jenner",
     shortName: "Jenner",
     mechClass: MechClasses[1],
-  },  
+  },
   Panther: {
     fullName: "Panther",
     shortName: "Panther",
     mechClass: MechClasses[1],
-  },  
+  },
   Mercury: {
     fullName: "Mercury",
     shortName: "Mercury",
     mechClass: MechClasses[1],
-  },  
+  },
   Urban: {
     fullName: "Urban Mech",
     shortName: "Urban",
     mechClass: MechClasses[1],
-  },  
+  },
   Raven: {
     fullName: "Raven",
     shortName: "Raven",
@@ -34,6 +34,21 @@ export default {
   Sentinel: {
     fullName: "Sentinel",
     shortName: "Sentinel",
+    mechClass: MechClasses[1],
+  },
+  Wolfshound: {
+    fullName: "Wolfshound",
+    shortName: "Wolfshound",
+    mechClass: MechClasses[1],
+  },
+  Locust: {
+    fullName: "Locust",
+    shortName: "Locust",
+    mechClass: MechClasses[1],
+  },
+  Commando: {
+    fullName: "Commando",
+    shortName: "Commando",
     mechClass: MechClasses[1],
   },
   Wolverine: {
@@ -91,6 +106,41 @@ export default {
     shortName: "Catapult",
     mechClass: MechClasses[3],
   },
+  Exterminator: {
+    fullName: "Exterminator",
+    shortName: "Exterminator",
+    mechClass: MechClasses[3],
+  },
+  Warhammer: {
+    fullName: "Warhammer",
+    shortName: "Warhammer",
+    mechClass: MechClasses[3],
+  },
+  Crusader: {
+    fullName: "Crusader",
+    shortName: "Crusader",
+    mechClass: MechClasses[3],
+  },
+  Rifleman: {
+    fullName: "Rifleman",
+    shortName: "Rifleman",
+    mechClass: MechClasses[3],
+  },
+  Grashopper: {
+    fullName: "Grashopper",
+    shortName: "Grashopper",
+    mechClass: MechClasses[3],
+  },
+  BlackKnight: {
+    fullName: "Black Knight",
+    shortName: "BlackKnight",
+    mechClass: MechClasses[3],
+  },
+  Thunderbolt: {
+    fullName: "Thunderbolt",
+    shortName: "Thunderbolt",
+    mechClass: MechClasses[3],
+  },
   Atlas: {
     fullName: "Atlas",
     shortName: "Atlas",
@@ -99,6 +149,36 @@ export default {
   KingCrab: {
     fullName: "KingCrab",
     shortName: "KingCrab",
+    mechClass: MechClasses[4],
+  },
+  Banshee: {
+    fullName: "Banshee",
+    shortName: "Banshee",
+    mechClass: MechClasses[4],
+  },
+  Highlander: {
+    fullName: "Highlander",
+    shortName: "Highlander",
+    mechClass: MechClasses[4],
+  },
+  Awesome: {
+    fullName: "Awesome",
+    shortName: "Awesome",
+    mechClass: MechClasses[4],
+  },
+  Battlemaster: {
+    fullName: "Battlemaster",
+    shortName: "Battlemaster",
+    mechClass: MechClasses[4],
+  },
+  Victor: {
+    fullName: "Victor",
+    shortName: "Victor",
+    mechClass: MechClasses[4],
+  },
+  Marauder2: {
+    fullName: "MarauderII",
+    shortName: "MarauderII",
     mechClass: MechClasses[4],
   },
 };
